b900a38975554f28b18913057c3da6e949104b33
Willy <guillecg13@gmail.com>
2023-07-13 17:56:12 +0200

    Added: config view pager adapter

---
+++ app/src/main/java/com/example/tabsviewpagerexample/ViewPagerAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.tabsviewpagerexample
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class ViewPagerAdapter(supportFragmentManager: FragmentManager) :
+    FragmentStatePagerAdapter(supportFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    private val mFragmentList = ArrayList<Fragment>()
+    private val mFragmentTitleList = ArrayList<String>()
+
+    override fun getCount(): Int = mFragmentList.size
+
+    override fun getItem(position: Int): Fragment = mFragmentList[position]
+
+    override fun getPageTitle(position: Int): CharSequence? = mFragmentTitleList[position]
+
+    fun addFragment(fragment: Fragment, title: String) {
+        mFragmentList.add(fragment)
+        mFragmentTitleList.add(title)
+    }
+}
