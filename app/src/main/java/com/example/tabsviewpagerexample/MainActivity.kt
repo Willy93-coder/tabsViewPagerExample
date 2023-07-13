c1c708719092a65bb04a1b1e617ff8f04d34ea52
Willy <guillecg13@gmail.com>
2023-07-13 17:51:29 +0200

    Added: 3 fragments and set up viewPager

--- app/src/main/java/com/example/tabsviewpagerexample/MainActivity.kt
+++ app/src/main/java/com/example/tabsviewpagerexample/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.tabsviewpagerexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tabsviewpagerexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Se carga la toolbar
+        setSupportActionBar(binding.toolbar)
+
+        // Se crea el adapter
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+
+        // Se añaden los fragments y los titulos de pestañas
+        adapter.addFragment(PersonsFragment(), "Personas")
+        adapter.addFragment(FruitsFragment(), "Frutas")
+        adapter.addFragment(AnimalsFragment(), "Animales")
+
+        // Se asocia el adapter
+        binding.viewPager.adapter = adapter
+
+        // Se cargan las tabs
+        binding.tabs.setupWithViewPager(binding.viewPager)
+
     }
 }
